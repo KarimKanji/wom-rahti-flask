55638e454b2ca6474cbc96215c9341da7d243750
KarimKanji <kanjikar@arcada.fi>
2022-10-14 12:18:11 +0000

    asd

--- app.py
+++ app.py
@@ -190,8 +190,8 @@ def index():
 def users():
     if request.method == 'POST':
         body = request.get_json()
-        print(request)
-        users = requests.post('https://wom22-projekt2-kanjikar-fallstrs.azurewebsites.net/users/login', json = {'email': body.email, 'password': body.password})
+
+        users = requests.post('https://wom22-projekt2-kanjikar-fallstrs.azurewebsites.net/users/login', json = {'email': 'body.email', 'password': 'body.password'})
         print(users.json())
         return users
 
